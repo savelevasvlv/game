bde80864d8982446ec2fbe184e17f0142ca18601
Angelina Saveleva <saveleva.svlv@gmail.com>
2021-07-08 00:38:09 -0400

    change directions and names

--- js/start.js
+++ js/start.js
@@ -4,7 +4,7 @@ document.querySelector('button').onclick = function () {
     let password = document.querySelector('#password_form').value;
     console.log(fullName, date, password)
     if (fullName === 'Rocco De Lutiis' && date === '1996-09-16' && password === 'Angelina') {
-        window.location.replace("index.html");
+        window.location.replace("start.html");
     } else {
         alert('Sorry!It seems that you are not Rocco De Lutiis!')
     }
