f645e2d8311d05bff12ad7d59d71b58c9491cdbc
Angelina <54841094+savelevasvlv@users.noreply.github.com>
2021-07-07 21:45:16 -0400

    Add files via upload

---
+++ css/days/quiz.js
@@ -0,0 +1,281 @@
+const headElem = document.getElementById("head");
+const buttonsElem = document.getElementById("buttons");
+const pagesElem = document.getElementById("pages");
+
+    
+
+
+//Класс, который представляет сам тест
+class Quiz
+{
+	constructor(type, questions, results)
+	{
+		//Тип теста: 1 - классический тест с правильными ответами, 2 - тест без правильных ответов
+		this.type = type;
+
+		//Массив с вопросами
+		this.questions = questions;
+
+		//Массив с возможными результатами
+		this.results = results;
+
+		//Количество набранных очков
+		this.score = 0;
+
+		//Номер результата из массива
+		this.result = 0;
+
+		//Номер текущего вопроса
+		this.current = 0;
+	}
+
+	Click(index)
+	{
+		//Добавляем очки
+		let value = this.questions[this.current].Click(index);
+		this.score += value;
+
+		let correct = -1;
+
+		//Если было добавлено хотя одно очко, то считаем, что ответ верный
+		if(value >= 1)
+		{
+			correct = index;
+		}
+		else
+		{
+			//Иначе ищем, какой ответ может быть правильным
+			for(let i = 0; i < this.questions[this.current].answers.length; i++)
+			{
+				if(this.questions[this.current].answers[i].value >= 1)
+				{
+					correct = i;
+					break;
+				}
+			}
+		}
+
+		this.Next();
+
+		return correct;
+	}
+
+	//Переход к следующему вопросу
+	Next()
+	{
+		this.current++;
+		
+		if(this.current >= this.questions.length) 
+		{
+			this.End();
+		}
+	}
+
+	//Если вопросы кончились, этот метод проверит, какой результат получил пользователь
+	End()
+	{
+		for(let i = 0; i < this.results.length; i++)
+		{
+			if(this.results[i].Check(this.score))
+			{
+				this.result = i;
+			}
+		}
+	}
+} 
+
+//Класс, представляющий вопрос
+class Question 
+{
+	constructor(text, answers)
+	{
+		this.text = text; 
+		this.answers = answers; 
+	}
+
+	Click(index) 
+	{
+		return this.answers[index].value; 
+	}
+}
+
+//Класс, представляющий ответ
+class Answer 
+{
+	constructor(text, value) 
+	{
+		this.text = text; 
+		this.value = value; 
+	}
+}
+
+//Класс, представляющий результат
+class Result 
+{
+	constructor(text, value)
+	{
+		this.text = text;
+		this.value = value;
+	}
+
+	//Этот метод проверяет, достаточно ли очков набрал пользователь
+	Check(value)
+	{
+		if(this.value <= value)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
+}
+
+//Массив с результатами
+const results = 
+[
+	new Result("No comments", 0),
+	new Result("Are you serious?", 2),
+	new Result("Almost there! Try again", 4),
+	new Result("Great Job! Get your reward!", 5, document.getElementById("present").toShow())
+];
+
+//Массив с вопросами
+const questions = 
+[
+	new Question("The place where you met Angelina", 
+	[
+		new Answer("Anakeesta", 0),
+		new Answer("Russia", 0),
+		new Answer("Ober Gatlinburg", 1),
+		new Answer("Your dreams", 0)
+	]),
+
+	new Question("What is your special place with Angelina", 
+	[
+		new Answer("Bypass view", 1),
+		new Answer("Arcade", 0),
+		new Answer("Ober Housing", 0),
+		new Answer("Landshark", 0)
+	]),
+
+	new Question("What's the first place you come together?", 
+	[
+		new Answer("Social Security Office", 1),
+		new Answer("SkyBridge", 0),
+		new Answer("Best Italian", 0),
+		new Answer("Cinema", 0)
+	]),
+
+	new Question("How to say in Russian HELLO", 
+	[
+		new Answer("Poka", 0),
+		new Answer("Zopa", 0),
+		new Answer("Lublu", 0),
+		new Answer("Privet", 1)
+	]),
+
+	new Question("When is Angelina's birthday?", 
+	[
+		new Answer("24 of February", 0),
+		new Answer("23 of January", 0),
+		new Answer("14 of October", 0),
+		new Answer("24 of January", 1)
+	]),
+];
+
+//Сам тест
+const quiz = new Quiz(1, questions, results);
+
+Update();
+
+//Обновление теста
+function Update()
+{
+	//Проверяем, есть ли ещё вопросы
+	if(quiz.current < quiz.questions.length) 
+	{
+		//Если есть, меняем вопрос в заголовке
+		headElem.innerHTML = quiz.questions[quiz.current].text;
+
+		//Удаляем старые варианты ответов
+		buttonsElem.innerHTML = "";
+
+		//Создаём кнопки для новых вариантов ответов
+		for(let i = 0; i < quiz.questions[quiz.current].answers.length; i++)
+		{
+			let btn = document.createElement("button");
+			btn.className = "button";
+
+			btn.innerHTML = quiz.questions[quiz.current].answers[i].text;
+
+			btn.setAttribute("index", i);
+
+			buttonsElem.appendChild(btn);
+		}
+		
+		//Выводим номер текущего вопроса
+		pagesElem.innerHTML = (quiz.current + 1) + " / " + quiz.questions.length;
+
+		//Вызываем функцию, которая прикрепит события к новым кнопкам
+		Init();
+	}
+	else
+	{
+		//Если это конец, то выводим результат
+		buttonsElem.innerHTML = "";
+		headElem.innerHTML = quiz.results[quiz.result].text;
+		pagesElem.innerHTML = "Очки: " + quiz.score;
+	}
+}
+
+function Init()
+{
+	//Находим все кнопки
+	let btns = document.getElementsByClassName("button");
+
+	for(let i = 0; i < btns.length; i++)
+	{
+		//Прикрепляем событие для каждой отдельной кнопки
+		//При нажатии на кнопку будет вызываться функция Click()
+		btns[i].addEventListener("click", function (e) { Click(e.target.getAttribute("index")); });
+	}
+}
+
+function Click(index) 
+{
+	//Получаем номер правильного ответа
+	let correct = quiz.Click(index);
+
+	//Находим все кнопки
+	let btns = document.getElementsByClassName("button");
+
+	//Делаем кнопки серыми
+	for(let i = 0; i < btns.length; i++)
+	{
+		btns[i].className = "button button_passive";
+	}
+
+	//Если это тест с правильными ответами, то мы подсвечиваем правильный ответ зелёным, а неправильный - красным
+	if(quiz.type == 1)
+	{
+		if(correct >= 0)
+		{
+			btns[correct].className = "button button_correct";
+		}
+
+		if(index != correct) 
+		{
+			btns[index].className = "button button_wrong";
+		} 
+	}
+	else
+	{
+		//Иначе просто подсвечиваем зелёным ответ пользователя
+		btns[index].className = "button button_correct";
+	}
+
+	//Ждём секунду и обновляем тест
+	setTimeout(Update, 1000);
+}
---
+++ js/start.js
@@ -0,0 +1,11 @@
+document.querySelector('button').onclick = function () {
+    let fullName = document.querySelector('#input_form').value;
+    let date = document.querySelector('#date_form').value;
+    let password = document.querySelector('#password_form').value;
+    console.log(fullName, date, password)
+    if (fullName === 'Rocco De Lutiis' && date === '1996-09-16' && password === 'Angelina') {
+        window.location.replace("index.html");
+    } else {
+        alert('Sorry!It seems that you are not Rocco De Lutiis!')
+    }
+}
